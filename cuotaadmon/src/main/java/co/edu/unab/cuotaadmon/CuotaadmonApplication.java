9b17d084197ebc44b216cada58ebf18970e1e068
julisam9 <julisam9@gmail.com>
2022-10-19 23:21:17 -0500

    Creacion del Proyecto en VS code

---
+++ cuotaadmon/src/main/java/co/edu/unab/cuotaadmon/CuotaadmonApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unab.cuotaadmon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuotaadmonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuotaadmonApplication.class, args);
+	}
+
+}
---
+++ cuotaadmon/src/test/java/co/edu/unab/cuotaadmon/CuotaadmonApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unab.cuotaadmon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuotaadmonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
